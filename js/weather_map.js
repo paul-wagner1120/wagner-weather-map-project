7b0c437cd6df14d34610e4af11748242422237b8
Paul Wagner <p.wagner1120@icloud.com>
2022-12-02 15:12:42 -0600

    finished building daily forecast cards and updated js so that all html is created in js and then pushed to html doc

--- js/weather_map.js
+++ js/weather_map.js
@@ -9,29 +9,43 @@ let map = new mapboxgl.Map({
     center: [-93.054429, 34.508414]
 });
 
-$.get("https://api.openweathermap.org/data/2.5/weather", {
-    APPID: weatherMapKey,
-    q:     "Hot Springs, AR, US",
-    units: "imperial"
-}).done(function (data){
-    console.log(data);
-        let cardHead = data.name
-        let temps = data.main.temp
-        let description = data.weather[0].description
-        let windSpeed = data.wind.speed
-    $('.card-header').html(cardHead);
-    $('.currentTemp').html(temps);
-    $('.weatherDescription').html(description);
-    $('.windSpd').html(windSpeed);
+let cardContainer = $('#card-container')
+
+
+let lat = 34.5037;
+let long = -93.0552;
+$.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + weatherMapKey + "&units=imperial").done(function(data) {
+    let reports = data.list;
+    let html = ''
+    for(let i = 0; i < reports.length; i += 8) {
+        // should get 5 objects back
+        // console.log(reports[i]);
+
+        let cardHead = reports[i].dt_txt.split(' ');
+        let highTemp = reports[i].main.temp_max;
+        let lowTemp = reports[i].main.temp_min;
+        let iconCode = reports[i].weather[0].icon;
+        // let iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+        let weatherDescription = reports[i].weather[0].description;
+        let humid = reports[i].main.humidity;
+        let windSpeed = reports[i].wind.speed;
+        let pressure = reports[i].main.pressure;
+
+
+            html +=
+            '<div class="card text-center" style="width: 18rem;">' +
+                '<div class="card-header">' + cardHead[0] + '</div>' +
+                    '<ul class="list-group list-group-flush">' +
+                        '<li class="list-group-item"><span>' + highTemp + '*F / ' + lowTemp + '*F</span><br><img src="https://openweathermap.org/img/w/' + iconCode + '.png" alt="Weather Icon"></li>'+
+                        '<li class="list-group-item"><span>Description: ' + weatherDescription + '</span><br><span>Humidity: ' + humid + '%</span></li>'+
+                        '<li class="list-group-item">Wind Speed: ' + windSpeed + 'mph</li>'+
+                        '<li class="list-group-item">Pressure: ' + pressure + 'psi</li>'+
+                    '</ul>'+
+            '</div>'
+
+    }
+    cardContainer.html(html);
 
 
 });
 
-// $.get("https://api.openweathermap.org/data/2.5/forecast", {
-//     APPID: weatherMapKey,
-//     lat:    34.5037,
-//     lon:   -93.0552,
-//     units: "imperial"
-// }).done(function(data) {
-//     console.log('5 day forecast', data);
-// });
