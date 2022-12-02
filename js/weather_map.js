46356c4dd8323d966a33e2aabdbc8a67ba0faf9b
Paul Wagner <p.wagner1120@icloud.com>
2022-12-02 15:35:33 -0600

    cleaning up the js spacing and indents

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 mapboxgl.accessToken = mapBoxKey;
 let map = new mapboxgl.Map({
     container: 'map',
@@ -8,29 +7,25 @@ let map = new mapboxgl.Map({
     zoom: 10,
     center: [-93.054429, 34.508414]
 });
-
 let cardContainer = $('#card-container')
-
 
 let lat = 34.5037;
 let long = -93.0552;
 $.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + weatherMapKey + "&units=imperial").done(function(data) {
     let reports = data.list;
-    let html = ''
+    let html = '';
     for(let i = 0; i < reports.length; i += 8) {
         // should get 5 objects back
-        // console.log(reports[i]);
+        console.log(reports[i]);
 
         let cardHead = reports[i].dt_txt.split(' ');
         let highTemp = reports[i].main.temp_max;
         let lowTemp = reports[i].main.temp_min;
         let iconCode = reports[i].weather[0].icon;
-        // let iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
         let weatherDescription = reports[i].weather[0].description;
         let humid = reports[i].main.humidity;
         let windSpeed = reports[i].wind.speed;
         let pressure = reports[i].main.pressure;
-
 
             html +=
             '<div class="card text-center" style="width: 18rem;">' +
@@ -42,10 +37,7 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon
                         '<li class="list-group-item">Pressure: ' + pressure + 'psi</li>'+
                     '</ul>'+
             '</div>'
-
     }
     cardContainer.html(html);
-
-
 });
 
