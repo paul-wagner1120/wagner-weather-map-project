345579abe53a2e32034f602ed595f0fe410fee57
Paul Wagner <p.wagner1120@icloud.com>
2022-12-02 16:10:28 -0600

    cleaning up JS

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,4 @@
 "use strict";
-
 mapboxgl.accessToken = mapBoxKey;
 let map = new mapboxgl.Map({
     container: 'map',
@@ -8,7 +7,6 @@ let map = new mapboxgl.Map({
     center: [-93.054429, 34.508414]
 });
 let cardContainer = $('#card-container')
-
 let lat = 34.5037;
 let long = -93.0552;
 $.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + weatherMapKey + "&units=imperial").done(function(data) {
@@ -17,15 +15,14 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon
     for(let i = 0; i < reports.length; i += 8) {
         // should get 5 objects back
         console.log(reports[i]);
-
-        let cardHead = reports[i].dt_txt.split(' ');
-        let highTemp = reports[i].main.temp_max;
-        let lowTemp = reports[i].main.temp_min;
-        let iconCode = reports[i].weather[0].icon;
-        let weatherDescription = reports[i].weather[0].description;
-        let humid = reports[i].main.humidity;
-        let windSpeed = reports[i].wind.speed;
-        let pressure = reports[i].main.pressure;
+            let cardHead = reports[i].dt_txt.split(' ');
+            let highTemp = reports[i].main.temp_max;
+            let lowTemp = reports[i].main.temp_min;
+            let iconCode = reports[i].weather[0].icon;
+            let weatherDescription = reports[i].weather[0].description;
+            let humid = reports[i].main.humidity;
+            let windSpeed = reports[i].wind.speed;
+            let pressure = reports[i].main.pressure;
 
             html +=
             '<div class="card text-center" style="width: 18rem;">' +
