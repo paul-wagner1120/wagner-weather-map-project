8d930d7c021789cf1d73cda8fa35b187d4581ef6
Paul Wagner <p.wagner1120@icloud.com>
2022-12-02 17:09:45 -0600

    added clickable marker and functionality to update the forecast based on marker location

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,21 +8,32 @@ let map = new mapboxgl.Map({
     zoom: 10,
     center: [-93.054429, 34.508414]
 });
+
+let marker = new mapboxgl.Marker();
+
+function add_marker(event){
+    let coords = event.lngLat;
+    console.log('Lng:', coords.lng, 'Lat:', coords.lat);
+    marker.setLngLat(coords).addTo(map);
+    getWeather(coords.lat, coords.lng);
+}
+map.on('click', add_marker);
+
 //--variable referencing empty container in HTML doc--//
 let cardContainer = $('#card-container')
 
-
 let lat = 34.5037;
 let long = -93.0552;
-$.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + weatherMapKey + "&units=imperial").done(function(data) {
-    let reports = data.list;
-    let html = '';
+function getWeather(lat,long){
+    $.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long +"&appid=" + weatherMapKey + "&units=imperial").done(function(data) {
+        let reports = data.list;
+        let html = '';
 
-    for(let i = 0; i < reports.length; i += 8) {
-        // should get 5 objects back
-        console.log(reports[i]);
+        for(let i = 0; i < reports.length; i += 8) {
+            // should get 5 objects back
+            console.log(reports[i]);
 
-               //--variables storing data from object properties--//
+            //--variables storing data from object properties--//
             let cardHead = reports[i].dt_txt.split(' ');
             let highTemp = reports[i].main.temp_max;
             let lowTemp = reports[i].main.temp_min;
@@ -32,20 +43,20 @@ $.get("https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon
             let windSpeed = reports[i].wind.speed;
             let pressure = reports[i].main.pressure;
 
-
             //--building the forecast cards via "for" loop--//
             html +=
-            '<div class="card text-center" style="width: 18rem;">' +
+                '<div class="card text-center" style="width: 18rem;">' +
                 '<div class="card-header">' + cardHead[0] + '</div>' +
-                    '<ul class="list-group list-group-flush">' +
-                        '<li class="list-group-item"><span>' + highTemp + '*F / ' + lowTemp + '*F</span><br><img src="https://openweathermap.org/img/w/' + iconCode + '.png" alt="Weather Icon"></li>'+
-                        '<li class="list-group-item"><span>Description: ' + weatherDescription + '</span><br><span>Humidity: ' + humid + '%</span></li>'+
-                        '<li class="list-group-item">Wind Speed: ' + windSpeed + 'mph</li>'+
-                        '<li class="list-group-item">Pressure: ' + pressure + 'psi</li>'+
-                    '</ul>'+
-            '</div>'
-    }
-    //--pushing forecast cards into empty div--//
-    cardContainer.html(html);
-});
-
+                '<ul class="list-group list-group-flush">' +
+                '<li class="list-group-item"><span>' + highTemp + '*F / ' + lowTemp + '*F</span><br><img src="https://openweathermap.org/img/w/' + iconCode + '.png" alt="Weather Icon"></li>'+
+                '<li class="list-group-item"><span>Description: ' + weatherDescription + '</span><br><span>Humidity: ' + humid + '%</span></li>'+
+                '<li class="list-group-item">Wind Speed: ' + windSpeed + 'mph</li>'+
+                '<li class="list-group-item">Pressure: ' + pressure + 'psi</li>'+
+                '</ul>'+
+                '</div>'
+        }
+        //--pushing forecast cards into empty div--//
+        cardContainer.html(html);
+    });
+}
+getWeather(lat,long);
